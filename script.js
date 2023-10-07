9749f03558638de9542e776ce142c341ee120d33
Aleks <aleks.m.mitev@gmail.com>
2023-10-07 19:34:34 +0300

    Added appearing animations
    If you click:
    - Next - card appears from the right
    - Previous - card appears from the left

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const eclipseData = [
 
 let currentIndex = 0;
 const eclipseInfo = document.getElementById("eclipse-info");
+const card = document.querySelector(".card");
 
 function displayContent(index) {
     eclipseInfo.textContent = eclipseData[index];
@@ -16,6 +17,7 @@ function showNext() {
     if (currentIndex < eclipseData.length - 1) {
         currentIndex++;
         displayContent(currentIndex);
+        appearRightCard(); // Trigger the animation
     }
 }
 
@@ -23,17 +25,29 @@ function showPrev() {
     if (currentIndex > 0) {
         currentIndex--;
         displayContent(currentIndex);
+        appearLeftCard(); // Trigger the animation
     }
 }
 
-// Get the element with class "right" and add a click event listener
-const rightElement = document.querySelector(".right");
-rightElement.addEventListener("click", showNext);
+document.querySelector(".right").addEventListener("click", showNext);
+document.querySelector(".left").addEventListener("click", showPrev);
 
-// Get the element with class "left" and add a click event listener
-const leftElement = document.querySelector(".left");
-leftElement.addEventListener("click", showPrev);
+// Animations
+function appearRightCard() {
+    card.classList.remove("appearLeft"); // Remove the appearLeft class
+    card.classList.remove("appearRight");
+    card.offsetHeight; // Trigger reflow
+    card.classList.add("appearRight"); // Add the appearRight class
+}
 
-// Initialize with the first eclipse information
+function appearLeftCard() {
+    card.classList.remove("appearRight"); // Remove the appearRight class
+    card.classList.remove("appearLeft");
+    card.offsetHeight; // Trigger reflow
+    card.classList.add("appearLeft"); // Add the appearLeft class
+}
+
+
+// Initialize with the first information and make the card appear
 displayContent(currentIndex);
-
+appearRightCard();
