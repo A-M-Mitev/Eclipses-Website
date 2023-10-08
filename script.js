9d946140c6fd13c69924ab290bd29fd765e7edc3
Aleks <aleks.m.mitev@gmail.com>
2023-10-08 05:21:16 +0300

    Added all pages

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// Sample eclipse data, you can replace this with your content
+const eclipseData = [
+    "Затъмнението е астрономично явление, при което небесно тяло става невидимо (напълно или частично) за земния наблюдател.",
+    "Slide 2",
+    "Slide 3",
+    "Slide 4",
+    "Slide 5",
+];
+
 const solarEclipseData = [
     "Различни затъмнения:Терминът затъмнение най-често се използва за описание или наслънчево или на лунно затъмнение, но той може да се отнася и затакива събития извън системата Земя-Луна.kогато се наблюдава в точки в пространството, различни отповърхността на Земята, Слънцето може да бъде затъмнено от тела",
     "Slide 2",
@@ -15,28 +22,78 @@ const lunarEclipseData = [
     "Slide 5",
 ];
 
+const otherEclipseData = [
+    "Терминът затъмнение най-често се използва за описание или на слънчево или на лунно затъмнение, но той може да се отнася и за такива събития извън системата Земя-Луна.",
+    "Slide 2",
+    "Slide 3",
+    "Slide 4",
+    "Slide 5",
+];
+
+const historyEclipseData = [
+    "От древни времена се водят записи на слънчеви затъмнения. Датите на Eclipse могат да се използват за хронологично датиране на исторически записи.",
+    "Slide 2",
+    "Slide 3",
+    "Slide 4",
+    "Slide 5",
+];
+
+const statisticsEclipseData = [
+    "Цикли на затъмнението: Цикъл на затъмнение се осъществява, когато затъмненията в серия са разделени от определен интервал от време.",
+    "Slide 2",
+    "Slide 3",
+    "Slide 4",
+    "Slide 5",
+];
+
 const card = document.querySelector(".card");
 let currentIndex = 0;
 let arrayLength;
 let eclipseInfo
+const indexNow = document.getElementById("currentIndex");
+const cardsTotal = document.getElementById("totalCards");
 /* Checking which html is being used, in order to load the correct array of information */
 var currentPageTitle = document.title;
 if (currentPageTitle == "Solar eclipse") {
     eclipseInfo = document.getElementById("solar-eclipse-info");
     arrayLength = solarEclipseData.length;
-}else if (currentPageTitle == "Lunar eclipse") {
+} else if (currentPageTitle == "Lunar eclipse") {
         eclipseInfo = document.getElementById("lunar-eclipse-info");
         arrayLength = lunarEclipseData.length;
-    }
+    } else if (currentPageTitle == "Other types of eclipse") {
+            eclipseInfo = document.getElementById("other-eclipse-info");
+            arrayLength = otherEclipseData.length;
+        } else if (currentPageTitle == "History of eclipses") {
+                eclipseInfo = document.getElementById("history-eclipse-info");
+                arrayLength = historyEclipseData.length;
+            } else if (currentPageTitle == "Statistics eclipses") {
+                    eclipseInfo = document.getElementById("statistics-eclipse-info");
+                    arrayLength = statisticsEclipseData.length;
+                } else if (currentPageTitle == "Eclipse") {
+                        eclipseInfo = document.getElementById("eclipse-info");
+                        arrayLength = eclipseData.length;
+                    }
 
-
+// Set header content be the same as opened html title 
+const headerParagraph = document.querySelector(".header-paragraph");
+headerParagraph.textContent = document.title;
 
 function displayContent(index) {
     if (currentPageTitle == "Solar eclipse") {
         eclipseInfo.textContent = solarEclipseData[index];
-    }else if (currentPageTitle == "Lunar eclipse") {
-        eclipseInfo.textContent = lunarEclipseData[index];
-        }
+    } else if (currentPageTitle == "Lunar eclipse") {
+            eclipseInfo.textContent = lunarEclipseData[index];
+            } else if (currentPageTitle == "Other types of eclipse") {
+                    eclipseInfo.textContent = otherEclipseData[index];
+                    } else if (currentPageTitle == "History of eclipses") {
+                            eclipseInfo.textContent = historyEclipseData[index];
+                            } else if (currentPageTitle == "Statistics eclipses") {
+                                    eclipseInfo.textContent = statisticsEclipseData[index];
+                                    } else if (currentPageTitle == "Eclipse") {
+                                            eclipseInfo.textContent = eclipseData[index];
+                                            }
+    indexNow.textContent = index + 1;
+    cardsTotal.textContent = arrayLength;
 }
 
 function showNext() {
